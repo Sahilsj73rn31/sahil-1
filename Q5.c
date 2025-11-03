953f6e40f20ea7fe418a84228122dfad19438178
Sahilsj73rn31 <sahilsatpute1410@gmail.com>
2025-11-03 20:47:33 +0530

    Update Q5.c
     perfect ;)

--- Q5.c
+++ Q5.c
@@ -2,13 +2,26 @@
 int main()
 {
   int num,a;
+  int pro=1;
 
   printf("Please enter your number.\n");
-  scanf("%d",a);
+  scanf("%d",&a);
+  
+    if (a > 0)
+    {
+      
+      for(num=1; num <=a; num++)
+      {
+        pro*=num;
+    
+      }
+        printf("The factorial of the number '%d' is %d",  a, pro);
+    
+    }
+    else if (a==0)
+    {
+      printf("The factorial is 1.");
 
-  for(a=1; a <=1000000000; a++)
-  {
-    printf("The factorial of the number '%d' is %d",a, a*a++);
-  }
+    }
   return 0;
 }
