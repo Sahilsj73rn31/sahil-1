de6b8ff1c187a56af2e9482806779f0e7308d392
Sahilsj73rn31 <sahilsatpute1410@gmail.com>
2025-11-07 19:12:47 +0530

    Update Q18.c
    Fix string handling and palindrome check logic.

--- Q18.c
+++ Q18.c
@@ -1,5 +1,40 @@
 #include<stdio.h>
+#include<string.h>
+
+
 int main()
 {
-  char wor
+  char str[100], rev[100];
+  int i,len,flag=0;
+
+  printf("Please enter the string:\n");
+  scanf("%s",str);
+
+    len = strlen(str);
+
+
+    for(i=0;i<len/2;i++)
+    {
+      if(str[i] != str[len-i-1])
+      {
+        flag=1;
+        break;
+      }
+    }
+
+    if(flag==0){
+      printf("The word %s is a palindrome.\n",str);
+      
+    }
+    else
+    {
+      printf("The word %s is not a palindrome.\n",str);
+
+    }
+
+
+    return 0;
+
+
+
 }
