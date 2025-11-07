201b22ab3a290ede8d4f737135a5e13072595bee
Sahilsj73rn31 <sahilsatpute1410@gmail.com>
2025-11-07 19:16:43 +0530

    Create Q20.c
    This program sorts an array of integers using the bubble sort algorithm and prints the sorted array.

---
+++ Q20.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+  int n,i,j,temp;
+  int arr[10000];
+
+  printf("Please enter the number of elements you want:\n");
+  scanf("%d",&n);
+
+  printf("Enter %d elements\n",n);
+
+  for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+
+  }
+
+
+  for(i=0;i<n-1;i++){
+    for(j=0;j<n-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+
+  printf("The array in asending order is: \n");
+  for(i=0;i<n;i++){
+    printf("%d\n",arr[i]);
+
+  }
+
+ 
+
+  return 0;
+
+}
